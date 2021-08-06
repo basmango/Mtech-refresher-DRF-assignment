54a0125d657b79f8b13f8c0982053999122c26f3
bassam <bassampervez@gmail.com>
2021-08-07 00:17:16 +0530

    migration, removed city

--- supplychain/models.py
+++ supplychain/models.py
@@ -11,5 +11,4 @@ class Customer(models.Model):
     mango_requirement = models.IntegerField()
     x_coord = models.FloatField()
     y_coord = models.FloatField()    
-    city = models.CharField(max_length = 50)
     blacklisted = models.BooleanField(default=False);
